4ca8f903bcb46741ddfa7c910eafe9f157766353
Hizir Cagin Bektas <hizir-cagin.bektas@broadcom.com>
2022-06-30 17:06:01 +0200

    implementing the service changes to the samples

--- samples/python/src/main/model/search/SearchModel.py
+++ samples/python/src/main/model/search/SearchModel.py
@@ -2,8 +2,8 @@
 
 class SearchModel:
     def __init__(self, dataset, copy_book, search_query, limit=0, offset=0):
-        self.dataSet = dataset
-        self.copyBook = copy_book
+        self.dataset = dataset
+        self.copybook = copy_book
         self.searchQuery = search_query
         self.limit = limit
         self.offset = offset
